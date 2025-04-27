d16a5d094cd6cea36f199f29e5a0a9fb1204e45f
noxalux <noxalux@gmail.com>
2025-04-27 00:01:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('open');
   navLinks.classList.toggle('open');
 });
+
+// Scroll Navbar Effect
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
