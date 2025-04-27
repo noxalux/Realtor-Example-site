0a05c57e07b877af62da965896ae40bada1f4fa5
noxalux <noxalux@gmail.com>
2025-04-26 23:47:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+// Mobile Menu Toggle
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 
 menuIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
   menuIcon.classList.toggle('open');
+  navLinks.classList.toggle('open');
 });
