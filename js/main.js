fa1498d52a05a9324a51428c93465226eb1d68ee
noxalux <noxalux@gmail.com>
2025-04-27 00:19:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('open');
+  navLinks.classList.toggle('open');
+});
 
